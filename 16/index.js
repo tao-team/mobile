be0c926547a425a0624d753e7b5c4f0eb103bb05
Yan Novak (yann) <yann@ozon.ru>
2021-01-22 13:34:31 +0300

    [16]: Итоговая версия для экза

--- 16/index.js
+++ 16/index.js
@@ -2,35 +2,56 @@ function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function startGame() {
+function startGame(n_rolls) {
+    function run_step(step) {
+        if (step <= n_rolls) {
+            userScore += randomInteger(1, 6)
+            botScore += randomInteger(1, 6)
+
+            userScoreEl.innerHTML = `Your score: <b>${userScore}</b>`
+            botScoreEl.innerHTML = `Bot score: <b>${botScore}</b>`
+
+            playButton.onclick = () => run_step(step + 1)
+        }
+
+        if (step === n_rolls) {
+            let res;
+            if (userScore > botScore) {
+                res = userWin
+            } else if (userScore < botScore) {
+                res = botWin
+            } else {
+                res = draw
+            }
+            resultsEl.innerHTML = res;
+            resultsEl.classList.remove('hidden')
+
+            playButton.innerText = 'One more time!'
+            playButton.onclick = () => startGame(3)
+        }
+    }
     let userScore = 0,
         botScore = 0;
 
-    for (let i = 0; i < 3; i++){
-        userScore += randomInteger(1, 6)
-    }
+    const userScoreEl = document.getElementById('user_score')
+    const botScoreEl = document.getElementById('bot_score')
 
-    for (let i = 0; i < 3; i++){
-        botScore += randomInteger(1, 6)
-    }
+    playButton.innerText = 'Roll!'
+    resultsEl.classList.add('hidden')
 
-    const scores = `User score: ${userScore}\n` +
-                   `Bot score: ${botScore}`
-
-    if (userScore > botScore){
-        alert(scores + "\n" + userWin);
-    }
-    else if (userScore < botScore){
-        alert(scores + "\n" + botWin);
-    }
-    else {
-        alert(scores + "\n" + draw);
-    }
+    run_step(1)
 }
 
-const userWin = "Congrats, you have won";
-const botWin = "Sorry, bot has won, try one more time";
-const draw = "You had a draw";
+const userWin = "Congrats, you <b>win</b>!";
+const botWin = "Sorry, you <b>lost</b> :(";
+const draw = "You have a <b>draw</b>";
 
-document.getElementById('play_button').addEventListener('click', startGame)
-document.addEventListener('keypress', e => e.key === 'Enter' && startGame())
+const playButton = document.getElementById('play_button')
+const resultsEl = document.getElementById('results')
+
+resultsEl.innerHTML = draw;
+// такая игра с классами предотвращает прыганье кнопки
+resultsEl.classList.add('hidden')
+
+playButton.innerText = 'Start the Dice!'
+playButton.onclick = () => startGame(3)
